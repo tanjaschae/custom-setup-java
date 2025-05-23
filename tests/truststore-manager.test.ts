bcadff9cfcb517a4d7ea4d6f6470d16cbcb88aff
tanja <tschaefer.extern@gmail.com>
2025-05-23 13:27:39 +0200

    test

--- tests/truststore-manager.test.ts
+++ tests/truststore-manager.test.ts
@@ -3,29 +3,14 @@ import {checkRootCaInTruststore} from '../src/truststore-manager';
 import * as exec from '@actions/exec';
 import * as path from 'path';
 
-jest.mock('@actions/core', () => ({
-    info: jest.fn(),
-}));
-
-jest.mock('@actions/exec', () => ({
-    exec: jest.fn(),
-}));
-
-jest.mock('path', () => ({
-    join: jest.fn(),
-}));
+jest.mock('path');
+jest.mock('@actions/exec');
 
 describe('Function: checkRootCaInTruststore', () => {
     const originalEnv = process.env;
     // Prepare the environment for each test
     beforeEach(() => {
         jest.resetModules();
-        process.env = {...originalEnv};
-    });
-
-    // Restore the original environment after all tests have completed
-    afterAll(() => {
-        process.env = originalEnv;
     });
 
     it('should throw an error when JAVA_HOME is not set', async () => {
@@ -34,9 +19,15 @@ describe('Function: checkRootCaInTruststore', () => {
 
     it('should run keytool command to check root CA in truststore if JAVA_HOME is set', async () => {
         process.env.JAVA_HOME = 'java_home_dummy_path';
+
+        const expectedPath = 'java_home_dummy_path/lib/security/cacerts';
+        (path.join as jest.Mock).mockReturnValue(expectedPath);
+
         await checkRootCaInTruststore('alias');
 
         expect(path.join).toHaveBeenCalledWith(process.env.JAVA_HOME, 'lib', 'security', 'cacerts');
+        expect(path.join(process.env.JAVA_HOME, 'lib', 'security', 'cacerts')).toBe(expectedPath);
+
         expect(exec.exec).toHaveBeenCalledWith('keytool', [
             '-list',
             '-keystore', path.join(process.env.JAVA_HOME, 'lib', 'security', 'cacerts'),
